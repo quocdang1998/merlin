4471cfcdffbeac8823157b3de6ab6787645530ef
quocdang1998 <quocdang1998@gmail.com>
2022-10-10 05:10:38 +0200

    Separate host-device from cu file

--- src/merlin/array/parcel.cu
+++ src/merlin/array/parcel.cu
@@ -12,44 +12,6 @@
 
 namespace merlin {
 
-#ifdef __MERLIN_LIBCUDA__
-// Get element at a given C-contiguous index
-__cudevice__ float & array::Parcel::operator[](std::uint64_t index) {
-    // calculate index vector
-    intvec index_ = contiguous_to_ndim_idx(index, this->shape_);
-    // calculate strides
-    std::uint64_t strides = inner_prod(index_, this->strides_);
-    float * element_ptr = reinterpret_cast<float *>(reinterpret_cast<std::uintptr_t>(this->data_) + strides);
-    return *element_ptr;
-}
-
-// Get element at a given Nd index
-__cudevice__ float & array::Parcel::operator[](std::initializer_list<std::uint64_t> index) {
-    // initialize index vector
-    intvec index_(index);
-    // calculate strides
-    std::uint64_t strides = inner_prod(index_, this->strides_);
-    float * element_ptr = reinterpret_cast<float *>(reinterpret_cast<std::uintptr_t>(this->data_) + strides);
-    return *element_ptr;
-}
-
-// Copy to shared memory
-__cudevice__ void array::Parcel::copy_to_shared_mem(array::Parcel * share_ptr, void * shape_strides_ptr) {
-    // copy meta data
-    bool check_zeroth_thread = (threadIdx.x == 0) && (threadIdx.y == 0) && (threadIdx.z == 0);
-    if (check_zeroth_thread) {
-        share_ptr->data_ = this->data_;
-        share_ptr->ndim_ = this->ndim_;
-    }
-    __syncthreads();
-    // copy shape and strides
-    this->shape_.copy_to_shared_mem(&(share_ptr->shape_), reinterpret_cast<std::uint64_t *>(shape_strides_ptr));
-    this->strides_.copy_to_shared_mem(&(share_ptr->strides_),
-                                      reinterpret_cast<std::uint64_t *>(shape_strides_ptr)+this->ndim_);
-}
-#endif  // __MERLIN_LIBCUDA__
-
-#ifdef __MERLIN_DLLCUDA__
 // Default constructor
 array::Parcel::Parcel(void) {}
 
@@ -174,7 +136,5 @@ void array::Parcel::free_current_data(void) {
 array::Parcel::~Parcel(void) {
     this->free_current_data();
 }
-
-#endif  // __MERLIN_DLLCUDA__
 
 }  // namespace merlin
--- src/merlin/device/decorator.hpp
+++ src/merlin/device/decorator.hpp
@@ -13,16 +13,4 @@
     #define __cuhostdev__
 #endif
 
-// Define __MERLIN_LIBCUDA__ for compiling device static linking library
-#ifdef __MERLIN_BUILT_AS_STATIC__
-    #define __MERLIN_LIBCUDA__
-    #define __MERLIN_DLLCUDA__
-#else
-    #ifdef LIBMERLIN_STATIC
-        #define __MERLIN_LIBCUDA__
-    #else
-        #define __MERLIN_DLLCUDA__
-    #endif  // LIBMERLIN_STATIC
-#endif  // __MERLIN_BUILT_AS_STATIC__
-
 #endif  // MERLIN_DEVICE_DECORATOR_HPP_
