3eef159babd869d00e8b1403e2ce93ec92f2051c
quocdang1998 <quocdang1998@gmail.com>
2022-10-10 20:11:15 +0200

    Add configuration for setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,35 @@
-import os
+import os, sys
 
 from setuptools import Extension, setup
 from Cython.Build import cythonize
+from packaging.version import Version, parse
+
+# check Cython version
+import Cython
+v_cython = parse(Cython.__version__)
+v_require = Version("3.0a1")
+if (v_cython < v_require):
+    raise ImportError("Package must be compiled with Cython 3.0")
+
+# import variable from config.py (generated file by CMake)
+from config import *
+
+# construct Cython extension options
+module_dir = os.path.dirname(os.path.realpath(__file__))
+ext_options = dict()
+ext_options["include_dirs"] = [os.path.join(module_dir, "src")]
+ext_options["library_dirs"] = [os.path.join(module_dir, "build")]
+if (MERLIN_CUDA == "ON") and (MERLIN_LIBKIND == "SHARED"):
+    ext_options["libraries"] = ["merlin", "merlincuda"]
+else:
+    ext_options["libraries"] = ["merlin"]
+if (sys.platform == "Linux"):
+    ext_options["runtime_library_dirs"] = [os.path.join(module_dir, "build")]
 
 extensions = [
-    Extension("merlin", ["merlin/main.pyx"],
-              include_dirs=["src"],
-              library_dirs=["build"], libraries=["libmerlin.so", "merlincuda"],
-              runtime_library_dirs=["build"],
+    Extension("merlin.device", ["merlin/gpu_query.pyx"],
               define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-              language="c++"
-              )
+              language="c++", **ext_options)
 ]
 
 setup(name="merlin",
